5954ec73e7ae50ffdadab38bdc0e3ddb169a37b6
Dan Farrell <dfarrell@spscommerce.com>
2015-08-06 18:38:35 -0500

    On osx 10.4.4, it seems to require trying to reverse lookup 169.254.169.254

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash -v
 sudo ifconfig lo0 alias 169.254.169.254 255.255.255.255
+grep -F '169.254.169.254 localhost' /etc/hosts || echo '169.254.169.254 localhost' | sudo tee -a /etc/hosts
 # if you had ipfw or iptables, you wouldn't have to sudo the 
 # exec to open port 80.
 # sudo ipfw add 100 fwd 127.00.1,65001 tcp from any to any 80 in

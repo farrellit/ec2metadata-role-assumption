fdb63c9a51cf8f5302dedfcfbaa05801df5e2243
Dan Farrell <dfarrell@spscommerce.com>
2016-11-02 14:31:42 -0500

    change 'pass inet' to 'pass on lo0 inet'
    Possible security improvement, though not sure what will really stop osx from
    routing from some other interface to lo0 169.254.169.254

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ sudo ifconfig lo0 alias 169.254.169.254 255.255.255.255
 grep -F '169.254.169.254 localhost' /etc/hosts || echo '169.254.169.254 localhost' | sudo tee -a /etc/hosts
 # How to stop routing through here if someone on the non-local network should ask?
 # Docker doesn't connect through 'lo', but it's default route leads to 
+#echo "
+#rdr pass inet proto tcp from any to 169.254.169.254 port 80 -> 127.0.0.1 port 8009
+#" | sudo pfctl -ef -
+
 echo "
-rdr pass inet proto tcp from any to 169.254.169.254 port 80 -> 127.0.0.1 port 8009
+rdr pass on lo0 inet proto tcp from any to 169.254.169.254 port 80 -> 127.0.0.1 port 8009
 " | sudo pfctl -ef -
